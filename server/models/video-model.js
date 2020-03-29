d4a9e2a485741c909919ce4cc19827afd3702631
gee <special.geeee@gmail.com>
2020-03-29 01:45:41 +0100

    add video database model

---
+++ server/models/video-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Video = new Schema(
+    {
+        title: { type: String, required: true },
+        id: { type: [String], required: true },
+        views: { type: Number, required: true },
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('videos', Movie)

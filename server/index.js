e37795e05ad7d99ed90edcd7f2168e4790eda077
gee <special.geeee@gmail.com>
2020-04-01 07:23:56 +0200

    added auth API entry points to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
+require('rootpath')();
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+//const path = require('path');
 const cors = require('cors');
+
+const errorHandler = require('helpers/error-handler');
 
 const db = require('./db');
 const videoRouter = require('./routes/video-router');
@@ -8,9 +14,9 @@ const videoRouter = require('./routes/video-router');
 const app = express();
 const apiPort = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -18,6 +24,12 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+// api routes
+app.use('/users', require('./users/users.controller'));
 app.use('/api', videoRouter);
 
+// global error handler
+app.use(errorHandler);
+
+// start server
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));

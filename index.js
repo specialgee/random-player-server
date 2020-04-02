f2bbfaed6857988a56db2d50e7cb70b45f714f2e
gee <special.geeee@gmail.com>
2020-04-02 02:27:12 +0200

    use environment PORT variable if available on server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = require('./db');
 const videoRouter = require('./routes/video-router');
 
 const app = express();
-const apiPort = 3000;
+const apiPort = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());

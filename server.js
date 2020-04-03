f646255d83241187db526eb5f3add44b87f97b9b
gee <special.geeee@gmail.com>
2020-04-03 04:09:47 +0200

    update server

--- server.js
+++ server.js
@@ -3,33 +3,41 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const path = require('path');
 const cors = require('cors');
 
-const errorHandler = require('helpers/error-handler');
-
-const db = require('./db');
 const videoRouter = require('./routes/video-router');
 
-const app = express();
 const apiPort = process.env.PORT || 3000
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+const db = require('./db');
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.post("/", (req, res) => {
+  res.send("POST request");
+});
+
 // api routes
-app.use('/users', require('./users/users.controller'));
 app.use('/api', videoRouter);
+app.use('/users', require('./users/users.controller'));
 
 // global error handler
+const errorHandler = require('helpers/error-handler');
 app.use(errorHandler);
 
 // start server
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+//app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+
+// listen for requests
+let listener = app.listen(apiPort, () => {
+  console.log("Server running on port " + listener.address().port);
+});
+

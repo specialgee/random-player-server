674c06dd0a8ada7d920b0cd32f3fe932198ec466
gee <special.geeee@gmail.com>
2020-05-21 01:54:32 +0200

    make project directory static

--- routes/upload-router.js
+++ routes/upload-router.js
@@ -51,8 +51,7 @@ const router = express.Router();
 let User = require('../models/User');
 
 router.post('/user-profile', upload.single('profileImg'), (req, res, next) => {
-    //const url = req.protocol + '://' + req.get('host')
-    const url = "https://random-player-server.herokuapp.com";
+    const url = req.protocol + '://' + req.get('host')
     const user = new User({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,

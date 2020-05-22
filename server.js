f7c6c37897d2a533cee2884c5e9a878e3c9e8338
gee <special.geeee@gmail.com>
2020-05-22 23:22:07 +0200

    removed cors options

--- server.js
+++ server.js
@@ -5,31 +5,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const apiPort = process.env.PORT || 3000
-
-const app = express();
-
-app.use(cors());
-app.options('*', cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 const videoRouter = require('./routes/video-router');
 const categoryRouter = require('./routes/category-router');
 
-// // CORS proxy
-// app.post('/api/category', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+const apiPort = process.env.PORT || 3000
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//   next();
-// });
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 // connect to mongoDB
 const db = require('./db');

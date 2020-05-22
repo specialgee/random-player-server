faebd00c182a0cf2c7fc7b9fd7b0045710b6ca17
gee <special.geeee@gmail.com>
2020-05-22 23:36:29 +0200

    :sparkles:

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const videoRouter = require('./routes/video-router');
-const categoryRouter = require('./routes/category-router');
-
 const apiPort = process.env.PORT || 3000
 
 const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+
+const videoRouter = require('./routes/video-router');
+const categoryRouter = require('./routes/category-router');
 
 // connect to mongoDB
 const db = require('./db');
